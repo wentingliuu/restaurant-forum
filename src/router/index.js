1121fd22aac2a5c3f45027d349f21a00317c2332
wentingliu <cindy810317@gmail.com>
2022-02-18 17:09:25 +0800

    add signup vue

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,11 @@ export default new Router({
       component: SignIn
     },
     {
+      path: '/signup',
+      name: 'sign-up',
+      component: () => import('../views/SignUp.vue') // 動態載入
+    },
+    {
       path: '*',
       name: 'not-found',
       component: NotFound
