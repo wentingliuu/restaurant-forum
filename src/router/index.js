0a73c4e6da534f2a60dca9580b993fa8b1cfa302
wentingliu <cindy810317@gmail.com>
2022-02-19 23:18:56 +0800

    add restaurant routes & pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import NotFound from '../views/NotFound.vue'
 import SignIn from '../views/SignIn.vue'
+import Restaurants from '../views/Restaurants.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'root',
+      redirect: '/restaurants'
+    },
     {
       path: '/signin',
       name: 'sign-in',
@@ -16,6 +22,26 @@ export default new Router({
       path: '/signup',
       name: 'sign-up',
       component: () => import('../views/SignUp.vue') // 動態載入
+    },
+    {
+      path: '/restaurants',
+      name: 'restaurants',
+      component: Restaurants
+    },
+    {
+      path: '/restaurants/feeds',
+      name: 'restaurants-feeds',
+      component: () => import('../views/RestaurantsFeeds.vue')
+    },
+    {
+      path: '/restaurants/top',
+      name: 'restaurants-top',
+      component: () => import('../views/RestaurantsTop.vue')
+    },
+    {
+      path: '/users/top',
+      name: 'users-top',
+      component: () => import('../views/UsersTop.vue')
     },
     {
       path: '*',
