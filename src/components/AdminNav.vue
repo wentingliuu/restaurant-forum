69f750637ef958437d81530a2ef9eadb95f23779
wentingliu <cindy810317@gmail.com>
2022-03-01 15:07:12 +0800

    add AdminCategories.vue view

--- src/components/AdminNav.vue
+++ src/components/AdminNav.vue
@@ -6,7 +6,11 @@
     >
       Restaurants
     </router-link> |
-    <a href="#">Categories</a> |
+    <router-link
+      to="/admin/categories"
+    >
+      Categories
+    </router-link> |
     <a href="#">Users</a>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -90,6 +90,11 @@ export default new Router({
       component: () => import('../views/AdminRestaurant.vue')
     },
     {
+      path: '/admin/categories',
+      name: 'admin-categories',
+      component: () => import('../views/AdminCategories.vue')
+    },
+    {
       path: '*',
       name: 'not-found',
       component: NotFound
